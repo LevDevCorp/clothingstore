1f412e60533b52534185d8035bcf323f070fd459
LevDevCorp <L.nats51@yahoo.com>
2020-08-18 22:29:32 +0400

    Updates

---
+++ precache-manifest.b7af6d5340adb7911e775c921f234b01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dabd8e3c89b91935ad076f38b13c3af6",
+    "url": "/clothingstore/index.html"
+  },
+  {
+    "revision": "4699034685c34efc3dd2",
+    "url": "/clothingstore/static/css/2.821b8ecc.chunk.css"
+  },
+  {
+    "revision": "c9b6d9550529495baa43",
+    "url": "/clothingstore/static/css/main.836a21d9.chunk.css"
+  },
+  {
+    "revision": "4699034685c34efc3dd2",
+    "url": "/clothingstore/static/js/2.6bd0d238.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/clothingstore/static/js/2.6bd0d238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9b6d9550529495baa43",
+    "url": "/clothingstore/static/js/main.29f0c849.chunk.js"
+  },
+  {
+    "revision": "b87266c6dba53d6a3d56",
+    "url": "/clothingstore/static/js/runtime-main.2e068c27.js"
+  },
+  {
+    "revision": "7adb1fb5d6a2c5fec98d9c90bcd1bcad",
+    "url": "/clothingstore/static/media/Arrow.7adb1fb5.svg"
+  }
+]);

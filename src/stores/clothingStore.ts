4b8e7be7bd291416c0196d172fc4133cd3f399c8
LevDevCorp <L.nats51@yahoo.com>
2020-08-18 23:01:44 +0400

    correction5

--- src/stores/clothingStore.ts
+++ src/stores/clothingStore.ts
@@ -6,27 +6,27 @@ class ClothingStore {
         const Clothing1 = new Clothing(1,1,"Men's Adidas Sports pants","white",53.99,'jpg');
         const Clothing2 = new Clothing(2,2,"sport-top","white",53.50,'jpg');
         const Clothing3 = new Clothing(3,3,"winter-jacket","whsite",29,'jpg');
-        const Clothing4 = new Clothing(4,4,"shoe-Nike-nizza","whsite",69.99,'jpg');
+        const Clothing4 = new Clothing(4,4,"Shoe-Nike-nizza","whsite",69.99,'jpg');
         
         const Clothing5 = new Clothing(5,5,"Men's Adidas Sports pants","white",53.99,'jpg');
         const Clothing6 = new Clothing(6,6,"sport-top","white",53.50,'jpg');
         const Clothing7 = new Clothing(7,7,"winter-jacket","whsite",29,'jpg');
-        const Clothing8 = new Clothing(8,8,"shoe-Nike-nizza","whsite",69.99,'jpg');
+        const Clothing8 = new Clothing(8,8,"Shoe-Nike-nizza","whsite",69.99,'jpg');
         
         const Clothing9 = new Clothing(9,9,"Men's Adidas Sports pants","white",53.99,'jpg');
         const Clothing10 = new Clothing(10,10,"sport-top","white",53.50,'jpg');
         const Clothing11 = new Clothing(11,3,"winter-jacket","whsite",29,'jpg');
-        const Clothing12 = new Clothing(12,4,"shoe-Nike-nizza","whsite",69.99,'jpg');
+        const Clothing12 = new Clothing(12,4,"Shoe-Nike-nizza","whsite",69.99,'jpg');
         
         const Clothing13 = new Clothing(13,2,"Men's Adidas Sports pants","white",53.99,'jpg');
         const Clothing14 = new Clothing(14,2,"sport-top","white",53.50,'jpg');
         const Clothing15 = new Clothing(15,3,"winter-jacket","whsite",29,'jpg');
-        const Clothing16 = new Clothing(16,4,"shoe-Nike-nizza","whsite",69.99,'jpg');
+        const Clothing16 = new Clothing(16,4,"Shoe-Nike-nizza","whsite",69.99,'jpg');
         
         const Clothing17 = new Clothing(17,2,"Men's Adidas Sports pants","white",53.99,'jpg');
         const Clothing18 = new Clothing(18,2,"sport-top","white",53.50,'jpg');
         const Clothing19 = new Clothing(19,3,"winter-jacket","whsite",29,'jpg');
-        const Clothing20 = new Clothing(20,4,"shoe-Nike-nizza","whsite",69.99,'jpg');
+        const Clothing20 = new Clothing(20,4,"Shoe-Nike-nizza","whsite",69.99,'jpg');
         
         return [Clothing1,Clothing2,Clothing3,Clothing4,
             Clothing5,Clothing6,Clothing7,Clothing8,

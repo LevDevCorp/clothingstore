923d4fa05ee2b9ba32070507fb46f5589a828093
giokavto <37060070+giokavto@users.noreply.github.com>
2020-08-26 01:36:46 +0400

    add homepage carousel

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import NewRelease from './NewRelease';
+import Brands from './Brands';
 
 export default class HomePage extends React.Component {
 	state = { filterValue: '' }
@@ -18,6 +20,44 @@ export default class HomePage extends React.Component {
                         <Link to='catalog'><button className="mt-2">More</button></Link>
                     </div>
                 </div>
+				<div className="container">
+					<div className="row mt--1">
+						<div className="homepage-banner col-4">
+							<Link to="/" >
+                    		<img src="images/Homepage/menSection.png" alt="menSection" />
+							</Link>
+                    		<div className="homepage-banner-text text-center">
+                        		<p>men</p>
+                        		<Link to='#'></Link>
+                    		</div>
+
+               		 	</div>
+						<div className="homepage-banner col-4">
+							<Link to="/">
+                    		<img src="images/Homepage/womenSection.png" alt="womenSection" />
+							</Link>
+                    		<div className="homepage-banner-text text-center">
+                        		<p>women</p>
+                        		<Link to='#'></Link>
+                    		</div>
+
+               		 	</div>
+						<div className="homepage-banner col-4">
+							<Link to="/" >
+                    		<img src="images/Homepage/kidsSection.png" alt="kidsSection" />
+							</Link>
+                    		<div className="homepage-banner-text text-center">
+                        		<p>kids</p>
+                        		<Link to='#'></Link>
+                    		</div>
+
+               		 	</div>
+					</div>
+				</div>
+				<div className="">
+						<NewRelease />
+						<Brands />
+				</div>
 			</div>
 		)
 	}
---
+++ src/components/Homepage/NewRelease.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Clothing from '../../Clothing';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+export default class NewRelease extends React.Component {
+    render() {
+     var settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1
+    };
+        return (
+          <Container className="mt--100 ">
+            <div className="clearfix">
+              <h4 className="float-left">New Release</h4>
+              <Link className="float-right text-upercase" to="/" >View More</Link>
+            </div>
+           <Slider className="border p-carousel bg-collor" {...settings}>
+        <div className="height-320  ">
+          <img className="margin-R" src="images/Homepage/pants-sport.png" alt="pants-sport" />
+        </div>
+        <div className="height-320 ">
+          <img className="margin-R" src="images/Homepage/shoe-women-casual.png" alt="shoe-women-casualn" />
+        </div>
+        <div className="height-320 " >
+          <img src="images/Homepage/winter-jacket.png" alt="winter-jacket" />
+        </div>
+        <div className="height-320 ">
+          <img src="images/Homepage/jexxi-ring.png" alt="jexxi-ring" />
+        </div>
+        <div className="height-320 ">
+          <img className="" src="images/Homepage/pants-sport.png" alt="pants-sport" />
+        </div>
+        <div  className="height-320 ">
+          <img src="images/Homepage/shoe-women-casual.png" alt="shoe-women-casualn" />
+        </div>
+        <div  className="height-320 ">
+          <img src="images/Homepage/winter-jacket.png" alt="winter-jacket" />
+        </div>
+        <div  className="height-320">
+          <img src="images/Homepage/jexxi-ring.png" alt="jexxi-ring" />
+        </div>
+      </Slider>
+      </Container>
+        )
+    }
+}

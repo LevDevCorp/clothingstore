2c30b896f6f155706360657fe878bceae5566594
LevDevCorp <L.nats51@yahoo.com>
2020-09-29 01:57:53 +0400

    debbuging2

--- src/stores/clothingStore.ts
+++ src/stores/clothingStore.ts
@@ -3,6 +3,7 @@ import Clothing from '../Clothing'
 class ClothingStore {
 
     createClothings = () =>{
+        
         const Clothing1 = new Clothing(1,1,"Men's Adidas Sports pants","white","Men","Pants",53.99,1,'jpg');
         const Clothing2 = new Clothing(2,2,"sport-top","white","Men","Tops&Tees",53.50,1,'jpg');
         const Clothing3 = new Clothing(3,3,"women-winter-jacket","white","Women","Jackets&Coats",29,1,'jpg');
@@ -22,12 +23,44 @@ class ClothingStore {
         const Clothing17 = new Clothing(17,4,"women-jewelry-silverbracelet","white","Women","Accessories",69.99,1,'jpg');
         const Clothing18 = new Clothing(18,4,"women-Sterling-Silver-Necklace-owl","white","Women","Accessories",25.99,1,'jpg');
         const Clothing19 = new Clothing(19,4,"women-top","white","Women","Tops&Tees",35.99,1,'jpg');
+        const Clothing20 = new Clothing(20,4,"blue-shirt","white","Men","Shirts",35.99,1,'jpg');
+        const Clothing21 = new Clothing(21,4,"dark-blue-shirt","white","Men","Shirts",35.99,1,'jpg');
+        const Clothing22 = new Clothing(22,4,"sweatshirt","white","Men","Hoodies&Sweatshirts",35.99,1,'jpg');
+        const Clothing23 = new Clothing(23,4,"blue-sweatshirt","white","Men","Hoodies&Sweatshirts",35.99,1,'jpg');
+        const Clothing24 = new Clothing(24,4,"dark-blue-sweatshirt","white","Men","Hoodies&Sweatshirts",35.99,1,'jpg');
+        const Clothing25 = new Clothing(25,4,"white-shirt","white","Men","Hoodies&Sweatshirts",35.99,1,'jpg');
+        const Clothing26 = new Clothing(26,4,"black-top","white","Men","Tops&Tees",35.99,1,'jpg');
+        const Clothing27 = new Clothing(27,4,"Parkas-Hooded-coat","white","Men","Jackets&Coats",35.99,1,'jpg');
+        const Clothing28 = new Clothing(28,4,"puma-coat","white","Men","Jackets&Coats",35.99,1,'jpg');
+        const Clothing29 = new Clothing(29,4,"Alpinestars-Winter-Jacket","white","Men","Jackets&Coats",35.99,1,'jpg');
+        const Clothing30 = new Clothing(30,4,"Hip-Hop-Streetwear","white","Men","Jackets&Coats",35.99,1,'jpg');
+        const Clothing31 = new Clothing(31,4,"Men-Joggers-Oversize","white","Men","Pants",35.99,1,'jpg');
+        const Clothing32 = new Clothing(32,4,"Vangull-Leather-Jacket","white","Women","Jackets&Coats",35.99,1,'jpg');
+        const Clothing33 = new Clothing(33,4,"hooded-Coats-Casual","white","Women","Jackets&Coats",35.99,1,'jpg');
+        const Clothing34 = new Clothing(34,4,"red-autumn-hoodie","white","Women","Hoodies&Sweatshirts",35.99,1,'jpg');
+        const Clothing35 = new Clothing(35,4,"Elegant-Drawstring-Sweet","white","Women","Hoodies&Sweatshirts",35.99,1,'jpg');
+        const Clothing36 = new Clothing(36,4,"grey-Oversize-Hooded","white","Women","Hoodies&Sweatshirts",35.99,1,'jpg');
+        const Clothing37 = new Clothing(36,4,"Gothic-Pants-Plaid","white","Women","Pants",35.99,1,'jpg');
+        const Clothing38 = new Clothing(36,4,"Harajuku-Ladies-Joggers","white","Women","Pants",35.99,1,'jpg');
+        const Clothing39 = new Clothing(36,4,"Ladies-Office-Work-Pants","white","Women","Pants",35.99,1,'jpg');
+        const Clothing40 = new Clothing(36,4,"High-Heels-Party","white","Women","Shoes",35.99,1,'jpg');
+        const Clothing41 = new Clothing(36,4,"Shirt-Ladies-Floral","white","Women","Shirts",35.99,1,'jpg');
+        const Clothing42 = new Clothing(36,4,"Office-Lady-Blouse","white","Women","Shirts",35.99,1,'jpg');
+        const Clothing43 = new Clothing(36,4,"Autumn-Chiffon-Blouse","white","Women","Shirts",35.99,1,'jpg');
+        const Clothing44 = new Clothing(36,4,"Spring-Lace-Hollow","white","Women","Tops&Tees",35.99,1,'jpg');
         
         return [Clothing1,Clothing2,Clothing3,Clothing4,
             Clothing5,Clothing6,Clothing7,Clothing8,
             Clothing9,Clothing10,Clothing11,Clothing12,
             Clothing13,Clothing14,Clothing15,Clothing16,
-            Clothing17,Clothing18,Clothing19,
+            Clothing17,Clothing18,Clothing19,Clothing20,
+            Clothing21,Clothing22,Clothing23,Clothing24,
+            Clothing25,Clothing26,Clothing27,Clothing28,
+            Clothing29,Clothing30,Clothing31,Clothing32,
+            Clothing33,Clothing34,Clothing35,Clothing36,
+            Clothing37,Clothing38,Clothing39,Clothing40,
+            Clothing41,Clothing42,Clothing43,Clothing44,
+            
         ];
     }
 
